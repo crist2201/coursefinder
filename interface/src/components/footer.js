b172a6334f475c7d1f8861c592d3a83e5a99eb1d
crist2201 <cristiansalazar822@gmail.com>
2022-06-02 12:15:24 -0400

    Project UI, header and footer created

---
+++ interface/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../stylesheets/footer.css'
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <h3> Copyright @ 2022 </h3>
+    </div>
+  );
+}
+
+export default Footer
---
+++ interface/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <h1> Find Online Courses </h1>
+    </div>
+  );
+}
+
+export default Header
