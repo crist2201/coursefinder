939636455291edf1f5e3ad6412de9f4e781f9f8c
crist2201 <cristiansalazar822@gmail.com>
2022-06-06 19:46:51 -0400

    Changes in App,css and App.js files

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,12 +1,16 @@
-import { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import SearchBar from './components/search_bar';
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='course-container'>
+        <SearchBar />
+      </div>
       <Footer />
+
 
 
 
