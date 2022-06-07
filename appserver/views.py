320f755f3164ab5e919b28c00cdd023fefbae74b
crist2201 <cristiansalazar822@gmail.com>
2022-06-07 13:20:29 -0400

    Create and configure /health endpoint

--- appserver/views.py
+++ appserver/views.py
@@ -1,12 +1,3 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpRequest
 # Create your views here.
-
-
-def healtCheck(request):
-    if request.path == "/health":
-        return HTTPResponse("OK")
-
-    return HttpResponse.status_code
--- server/settings.py
+++ server/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'server.health.HealthCheckMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
