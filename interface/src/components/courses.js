68ec50534eec99220c755a936e18e6bb4d900844
crist2201 <cristiansalazar822@gmail.com>
2022-06-22 17:39:32 -0400

    Courses card created

--- interface/src/components/courses.js
+++ interface/src/components/courses.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { DynamicStar } from 'react-dynamic-star';
 import '../stylesheets/courses.css'
 
 function Courses({ courses }) {
   const showCourse = courses.map((e, i) => {
     return (
-      <div className='courses-container' key={i}>
-        <h3 className='title'>{e.title}  </h3>
-        <h3>{e.platform}</h3>
-        <h3>{e.rating}</h3>
-        <h4>{e.instructor}</h4>
-        <p id='des'>{e.description}</p>
-        <p>{e.price}</p>
-      </div>
-    )
+      <div className='courses-container'>
+        <p className='description'>{e.description}</p>
+        <h3 className='instructor'>{e.instructor}</h3>
+        <p className='price'>$ {e.price}</p>
+        <a className='url' href={e.url}>Link to the course</a>
 
+        <div className='info-container'>
+          <h2 className='title'>{e.title}  </h2>
+          <h2 className='platform'> ({e.platform}) </h2>
+          {/* <h3 className='rating'>{e.rating}</h3> */}
+          <DynamicStar className='rating' rating={parseFloat(e.rating)} totalStars={5} outlined={true} width={20} height={20} />
+        </div>
+      </div>
+
+    )
   });
 
   return (
-    <div>
+    <div >
       {showCourse}
     </div>
   );
